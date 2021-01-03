8b056da97fe3bf3cfddd713390e5947fb5b75339
Alex Shelton <alexandershelton@icloud.com>
2021-01-03 00:57:23 -0500

    plotting of decision boundry

--- hw2/python/logistic_regression_single_feature.py
+++ hw2/python/logistic_regression_single_feature.py
@@ -4,7 +4,64 @@ import matplotlib.pyplot as plt
 '''optimization algo'''
 from scipy import optimize
 
-def plotData(data):
+def plotDecisionBoundary(plotData, theta, X, y):
+    """
+    Plots the data points X and y into a new figure with the decision boundary defined by theta.
+    Plots the data points with * for the positive examples and o for  the negative examples.
+    Parameters
+    ----------
+    plotData : func
+        A function reference for plotting the X, y data.
+    theta : array_like
+        Parameters for logistic regression. A vector of shape (n+1, ).
+    X : array_like
+        The input dataset. X is assumed to be  a either:
+            1) Mx3 matrix, where the first column is an all ones column for the intercept.
+            2) MxN, N>3 matrix, where the first column is all ones.
+    y : array_like
+        Vector of data labels of shape (m, ).
+    """
+    # make sure theta is a numpy array
+    theta = np.array(theta)
+
+    # Plot Data (remember first column in X is the intercept)
+    plotData(X[:, 1:3], y)
+
+    if X.shape[1] <= 3:
+        # Only need 2 points to define a line, so choose two endpoints
+        plot_x = np.array([np.min(X[:, 1]) - 2, np.max(X[:, 1]) + 2])
+
+        # Calculate the decision boundary line
+        plot_y = (-1. / theta[2]) * (theta[1] * plot_x + theta[0])
+
+        # Plot, and adjust axes for better viewing
+        plt.plot(plot_x, plot_y)
+
+        # Legend, specific for the exercise
+        plt.legend(['Admitted', 'Not admitted', 'Decision Boundary'])
+        plt.xlim([30, 100])
+        plt.ylim([30, 100])
+    else:
+        # Here is the grid range
+        u = np.linspace(-1, 1.5, 50)
+        v = np.linspace(-1, 1.5, 50)
+
+        z = np.zeros((u.size, v.size))
+        # Evaluate z = theta*x over the grid
+        for i, ui in enumerate(u):
+            for j, vj in enumerate(v):
+                z[i, j] = np.dot(mapFeature(ui, vj), theta)
+
+        z = z.T  # important to transpose z before calling contour
+        # print(z)
+
+        # Plot z = 0
+        plt.contour(u, v, z, levels=[0], linewidths=2, colors='g')
+        plt.contourf(u, v, z, levels=[np.min(z), 0, np.max(z)], cmap='Greens', alpha=0.4)
+
+
+def plotData(X,y):
+    data = np.c_[X,y]
     positives = data[data[:,2] == 1]
     negatives = data[data[:,2] == 0]
 
@@ -47,6 +104,8 @@ def costFunction(theta, X, y):
     
 
 
+# equivalent to MATLAB's fminunc
+# See https://stackoverflow.com/questions/18801002/fminunc-alternate-in-numpy
 def optimizeTheta(theta,X,y):
     '''MATLABS equiv of fminunc, runs a library optimization algorithm'''
     print(f'shape of X: {np.shape(X)}\nshape of y: {np.shape(y)}\nshape of theta: {np.shape(theta)}')
@@ -57,8 +116,9 @@ def optimizeTheta(theta,X,y):
                         jac=True,
                         method='TNC',
                         options=options)
-    print(f'Cost at theta foubd by optimize.minimize: {result.fun}')
-    return result['x']
+    print(f'Cost at theta found by optimize.minimize: {result.fun}')
+    return ( result['x'], result.fun )
+
 
 
 def main():
@@ -75,23 +135,33 @@ def main():
     
     print(f'Sizes of vectors: X: {np.shape(X)},  y: {np.shape(y)}')
     # Plotting the data:
-    #plotData(data)
-    #plt.draw()
+    plotData(X,y)
+    plt.draw()
     
     X = np.c_[np.ones((m,1)), X] # add bias (theta 0)   X = |1's|feature 1| feature 2|
     n = np.shape(X)[1]           # number of features in X
     theta = np.zeros((n))        # (features,) dimention vector 
+    
     # Computing cost and gradient:
     # should result in cost: 0.693, grad = [-0.1, -12.0092, -11.2628]
     cost, gradiant = costFunction(theta, X, y)
     print(f'cost at initial theta: {cost}')
     print(f'Gradiants for inintial theta(zeros): \n{gradiant}') 
     
-
-    theta = optimizeTheta(theta, X, y)
+    theta, cost = optimizeTheta(theta, X, y)
     print(f'Optimized theta values:\n {theta}')
+    plotDecisionBoundary(plotData, theta, X, y)
+    
+    # predict prob that a student with a score of 45 on exam1 and 85 on exam 2
+    prediction_vector = np.array([ 1, 45, 85])
+    prob = sigmoid(prediction_vector @ theta)
+    print(f'For a student with scores 45 and 85, we predict an admission probability of {prob}')
+    
+    p = prediction(theta, X) #compute accuracy on training set
+    print(f'Train accuracy: {}')
 
-    #plt.show() # keeping plotted window open 
+
+    plt.show() # keeping plotted window open 
     return 0
 
 
