ca72c96780d5729e73a5e8574f8919eca77c132a
Alex Shelton <alexandershelton@icloud.com>
2021-01-06 21:37:35 -0500

    logistic regression mnist

--- hw3/hw3.m
+++ hw3/hw3.m
@@ -20,3 +20,10 @@ lambda_t = 3;
 fprintf('Cost: %f | Expected cost: 2.534819\n',J);
 fprintf('Gradients:\n'); fprintf('%f\n',grad);
 fprintf('Expected gradients:\n 0.146561\n -0.548558\n 0.724722\n 1.398003\n');
+
+num_labels = 10; % 10 labels, from 1 to 10
+lambda = 0.1;
+[all_theta] = oneVsAll(X, y, num_labels, lambda);
+
+pred = predictOneVsAll(all_theta, X);
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
